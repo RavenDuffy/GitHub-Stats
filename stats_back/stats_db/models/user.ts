fc3f436d166618b1d52707dd049ebf3f5b6b19b8
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-04-24 19:52:28 +0100

    Update user.ts
    - not sure why I thought this would work without a local login

--- stats_back/stats_db/models/user.ts
+++ stats_back/stats_db/models/user.ts
@@ -2,16 +2,20 @@ import mongoose from 'mongoose'
 
 export interface User extends mongoose.Document {
     username: string,
+    gitUsername: string,
     displayName: string,
     avatar: string,
     accessToken: string | null
+    password: string
 }
 
 export const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
+    gitUsername: { type: String, required: true },
     displayName: { type: String, required: true },
     avatar: { type: String, required: true },
-    accessToken: { type: String, nullable: true}
+    accessToken: { type: String, nullable: true},
+    password: { type: String, required: true }
 })
 
 UserSchema.index({ accessToken: 1, username: 1 }, { unique: true })
