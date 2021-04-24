22f679b536c775df5406d6241c9513a29920ac51
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-04-24 19:53:07 +0100

    Update user.ts
    I take it back

--- stats_back/stats_db/models/user.ts
+++ stats_back/stats_db/models/user.ts
@@ -2,20 +2,16 @@ import mongoose from 'mongoose'
 
 export interface User extends mongoose.Document {
     username: string,
-    gitUsername: string,
     displayName: string,
     avatar: string,
     accessToken: string | null
-    password: string
 }
 
 export const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    gitUsername: { type: String, required: true },
     displayName: { type: String, required: true },
     avatar: { type: String, required: true },
-    accessToken: { type: String, nullable: true},
-    password: { type: String, required: true }
+    accessToken: { type: String, nullable: true}
 })
 
 UserSchema.index({ accessToken: 1, username: 1 }, { unique: true })
