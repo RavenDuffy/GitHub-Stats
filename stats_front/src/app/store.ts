a72f20c94a966297198523e50d90173a5f7cd3ce
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-06-25 13:26:05 +0100

    Wiping current frontend

--- stats_front/src/app/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
-
-import { reducers } from './reducers'
-
-const middleware = applyMiddleware(logger, thunk)
-
-export const store = createStore(reducers, middleware)
--- stats_front/src/pages/home/tokenSocket.ts
+++
@@ -1,3 +0,0 @@
-const sProt = window.location.protocol === 'https' ? 'wss:' : 'ws:'
-const sURL = `${sProt}//${window.location.hostname}:4010`
-export const socket = new WebSocket(sURL)
