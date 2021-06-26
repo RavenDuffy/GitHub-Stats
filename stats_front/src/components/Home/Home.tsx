38407227bed078c2f273e4a8eb9ca0548569e769
SAndersonCheung <summer.anderson99@hotmail.co.uk>
2021-06-26 21:30:57 +0100

    stat reveal page part one
    - moved button styling to global to minimise code reuse
    - styled the stat reveal page
    - added a download image button
    - added credit to stat reveal page
    
    x note for improvement: the css for the credit styling is on both login.module.scss and home.module.scss as it did not easily move to global

--- stats_front/src/components/Home/Home.tsx
+++ stats_front/src/components/Home/Home.tsx
@@ -1,12 +1,24 @@
+import styles from './home.module.scss'
 import { StatSVG } from "./StatSVG"
 
 export const Home = ({ stats }: any) => {
 
     return (
-        <>
+        <div className={styles.mainContent}>
             <h3>Hey there {stats.username}</h3>
-
+            <p>Thanks for waiting!<br/> Here's your GitHubStats image:</p>
             <StatSVG stats={stats}/>
-        </>
+            
+
+            <div className={styles.downloadButton}>
+                <button>Download Image</button>
+            </div>
+            
+            <div className={styles.extraInfo}>
+                <p>The card is cool.</p>
+            </div>
+
+            <p className={styles.credit}>By <a href="https://github.com/RavenDuffy">Raven Duffy</a> & <a href="https://github.com/SAndersonCheung">Summer Anderson-Cheung</a></p>
+        </div>
     )
 }
