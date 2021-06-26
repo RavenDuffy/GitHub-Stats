fe5f913066754a30b94f0d1379660ea8676b705d
SAndersonCheung <summer.anderson99@hotmail.co.uk>
2021-06-26 22:18:16 +0100

    MainContent Update 2
    - Added a wrapper to the 'home' page to center it
    - added guide on how to add stat image to github profile
    - styled the additional content

--- stats_front/src/components/Home/Home.tsx
+++ stats_front/src/components/Home/Home.tsx
@@ -5,21 +5,29 @@ import { StatSVG } from "./StatSVG"
 export const Home = ({ stats }: any) => {
 
     return (
-        <div className={styles.mainContent}>
-            <h3>Hey there {stats.username}</h3>
-            <p>Thanks for waiting!<br/> Here's your GitHubStats image:</p>
-            <StatSVG stats={stats}/>
-            
+        <div className={styles.mainWrapper}>
+            <div className={styles.mainContent}>
+                <h3>Hey there <span className={styles.username}>{stats.username}</span></h3>
+                <p>Thanks for waiting!<br/> Here's your <span className={styles.highlight}>GitHubStats</span> image:</p>
+                <StatSVG stats={stats}/>
+                
 
-            <div className={styles.downloadButton}>
-                <button>Download Image</button>
-            </div>
-            
-            <div className={styles.extraInfo}>
-                <p>The card is cool.</p>
-            </div>
+                <div className={styles.downloadButton}>
+                    <button>Download Image</button>
+                </div>
+                
+                <div className={styles.extraInfo}>
+                    <p>If you wish to display your <span className={styles.highlight}>GitHubStats</span> image on your GitHub profile, follow these easy steps: </p>
+                    <ol>
+                        <li>Create a public repository, that has the <span className={styles.bolden}>same name</span> as your GitHub username.</li>
+                        <li>Initialise that repository with a readme.md. The readme.md will automatically display on your profile.</li>
+                        <li>Place your <span className={styles.highlight}>GitHubStats</span> SVG into this readme.md</li>
+                        <li>Enjoy showing off your commits!</li>
+                    </ol>
+                </div>
 
-            <Credit />
+                <Credit />
+            </div>
         </div>
     )
 }
